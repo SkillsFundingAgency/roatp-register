49768a38c75e08e9c013b98ae44e86592cd56a16
Scott Cowan <info@scottcowan.org>
2017-03-23 14:01:11 +0000

    adding error logging and logging of the starting and stopping of the web role

--- src/Sfa.Roatp.Register.Web/Global.asax.cs
+++ src/Sfa.Roatp.Register.Web/Global.asax.cs
@@ -10,6 +10,7 @@ using System.Web.Http;
 using Sfa.Roatp.Register.Web;
 using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.Azure;
+using SFA.DAS.NLog.Logger;
 
 namespace Sfa.Roatp.Registry.Web
 {
@@ -18,9 +19,47 @@ namespace Sfa.Roatp.Registry.Web
         void Application_Start(object sender, EventArgs e)
         {
             // Code that runs on application startup
+            MvcHandler.DisableMvcResponseHeader = true;
+            var logger = DependencyResolver.Current.GetService<ILog>();
+
+            logger.Info("Starting Web Role");
+
             GlobalConfiguration.Configure(WebApiConfig.Register);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            TelemetryConfiguration.Active.InstrumentationKey = CloudConfigurationManager.GetSetting("InstrumentationKey") ?? string.Empty;
+            SetupApplicationInsights();
+
+            logger.Info("Web Role started");
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            Exception ex = Server.GetLastError().GetBaseException();
+            var logger = DependencyResolver.Current.GetService<SFA.DAS.NLog.Logger.ILog>();
+
+            if (ex is HttpException
+                && ((HttpException)ex).GetHttpCode() != 404
+                && !UrlContains("findatrainingorganisation.nas.apprenticeships.org.uk"))
+            {
+                logger.Error(ex, "App_Error");
+            }
+        }
+
+        private bool UrlContains(string text)
+        {
+            var url = HttpContext.Current.Request.RequestContext.HttpContext.Request.Url;
+            if (url == null)
+            {
+                return false;
+            }
+
+            return url.OriginalString.Contains(text);
+        }
+
+        private void SetupApplicationInsights()
+        {
+            TelemetryConfiguration.Active.InstrumentationKey = CloudConfigurationManager.GetSetting("InstrumentationKey");
+
+            TelemetryConfiguration.Active.TelemetryInitializers.Add(new ApplicationInsightsInitializer());
         }
     }
 }
---
+++ src/Sfa.Roatp.Register.Web/Logging/ApplicationInsightsInitializer.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.ApplicationInsights.Channel;
+
+namespace Sfa.Roatp.Registry.Web
+{
+    public sealed class ApplicationInsightsInitializer : Microsoft.ApplicationInsights.Extensibility.ITelemetryInitializer
+    {
+        public void Initialize(ITelemetry telemetry)
+        {
+            telemetry.Context.Properties["Application"] = "Sfa.Roatp.Web";
+        }
+    }
+}
