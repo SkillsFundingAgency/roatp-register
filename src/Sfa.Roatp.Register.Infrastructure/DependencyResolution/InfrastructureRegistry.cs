8b75b7152ca3725e86012743c55dd4137c0e380f
Scott Cowan <info@scottcowan.org>
2017-03-27 16:32:07 +0100

    adding version number to logging

--- src/Sfa.Roatp.Register.Infrastructure/DependencyResolution/InfrastructureRegistry.cs
+++ src/Sfa.Roatp.Register.Infrastructure/DependencyResolution/InfrastructureRegistry.cs
@@ -1,4 +1,7 @@
-﻿using Sfa.Das.ApprenticeshipInfoService.Infrastructure.Elasticsearch;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+using Sfa.Das.ApprenticeshipInfoService.Infrastructure.Elasticsearch;
 using Sfa.Roatp.Register.Core.Configuration;
 using Sfa.Roatp.Register.Core.Services;
 using Sfa.Roatp.Register.Infrastructure.Elasticsearch;
@@ -13,11 +16,28 @@ namespace Sfa.Roatp.Register.Infrastructure.DependencyResolution
     {
         public InfrastructureRegistry()
         {
-            For<ILog>().Use(x => new NLogLogger(x.ParentType, x.GetInstance<IRequestContext>())).AlwaysUnique();
+            For<ILog>().Use(x => new NLogLogger(
+                x.ParentType, 
+                x.GetInstance<IRequestContext>(),
+                GetProperties())).AlwaysUnique();
             For<IConfigurationSettings>().Use<ApplicationSettings>();
             For<IGetProviders>().Use<ProviderRepository>();
             For<IElasticsearchClientFactory>().Use<ElasticsearchClientFactory>();
             For<IElasticsearchCustomClient>().Use<ElasticsearchCustomClient>();
+        }
+
+        private IDictionary<string, object> GetProperties()
+        {
+            var properties = new Dictionary<string, object>();
+            properties.Add("Version", GetVersion());
+            return properties;
+        }
+
+        private string GetVersion()
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            FileVersionInfo fileVersionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
+            return fileVersionInfo.ProductVersion;
         }
     }
 }
