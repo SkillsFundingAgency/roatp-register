348f6262a38beacafadca6d27a71810c5ed9505a
Scott Cowan <info@scottcowan.org>
2017-05-19 16:13:28 +0100

    CI-306 fixing date format displayed on the page

---
+++ src/Sfa.Roatp.Register.Core/Utility/DateTimeExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Sfa.Roatp.Register.Core.Utility
+{
+    public static class DateTimeExtensions
+    {
+        /// <summary>
+        /// GDS format: d MMM yyyy
+        /// </summary>
+        /// <param name="date"></param>
+        /// <returns></returns>
+        public static string ToGdsFormat(this DateTime date)
+        {
+            return date.ToString("d MMM yyyy");
+        }
+
+        /// <summary>
+        /// GDS format: d MMMM yyyy
+        /// </summary>
+        /// <param name="date"></param>
+        /// <returns></returns>
+        public static string ToGdsFormatFull(this DateTime date)
+        {
+            return date.ToString("d MMMM yyyy");
+        }
+
+        public static string ToFullDateEntryFormat(this DateTime date)
+        {
+            return date.ToString("dd MM yyyy");
+        }
+
+        public static DateTime ToGMTStandardTime(this DateTime date)
+        {
+            return TimeZoneInfo.ConvertTime(date, TimeZoneInfo.FindSystemTimeZoneById("GMT Standard Time"));
+        }
+        public static string ToGdsFormatWithoutDayAbbrMonth(this DateTime date)
+        {
+            return date.ToString("MMM yyyy");
+        }
+        public static string ToGdsFormatWithoutDay(this DateTime date)
+        {
+            return date.ToString("MMMM yyyy");
+        }
+
+    }
+}
